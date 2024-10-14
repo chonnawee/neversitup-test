f803b2e2b3e7c6dd8759fcbc262e4e5058b1627a
Chonnawee <chonnawee.b25@gmail.com>
2024-10-14 15:03:18 +0700

    add router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
-	"time"
+	"encoding/json"
+	"neversitup-test/internal/core/implements/services/counter"
+	"neversitup-test/internal/core/implements/services/odd"
+	"neversitup-test/internal/core/implements/services/permutations"
+	"neversitup-test/internal/handlers"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-		}
-	}()
+var port string
 
-	select {}
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("cannot read environment variables")
+	}
+}
+
+func counterRoute(r *fiber.App) {
+	counterSrv := counter.NewCounterServiceImplement()
+	counterHdr := handlers.NewCounterHandler(counterSrv)
+	r.Get("/counter/smileys", counterHdr.CountSmileys)
+}
+
+func oddRoute(r *fiber.App) {
+	oddSrv := odd.NewOddServiceImplement()
+	oddHdr := handlers.NewOddHander(oddSrv)
+	r.Get("/odd/find-int", oddHdr.FindIntInSlice)
+}
+
+func permutationRoute(r *fiber.App) {
+	permutationsSrv := permutations.NewPermutationsServiceImplement()
+	permuhtaionsHdr := handlers.NewPermutationsHandler(permutationsSrv)
+	r.Get("/permutation", permuhtaionsHdr.Generate)
+}
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+		BodyLimit:   0,
+	})
+
+	permutationRoute(app)
+	oddRoute(app)
+	counterRoute(app)
+
+	app.Listen(":" + port)
 }
